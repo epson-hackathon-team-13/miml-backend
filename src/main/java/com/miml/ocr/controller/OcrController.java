03ced9508389142d2fc5d787e5a2e19d27526ad6
popup0221 <popup0221@naver.com>
2024-06-24 04:59:31 +0900

    refactor: request body type 변경

--- src/main/java/com/miml/ocr/controller/OcrController.java
+++ src/main/java/com/miml/ocr/controller/OcrController.java
@@ -3,11 +3,14 @@ package com.miml.ocr.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.miml.common.api.ApiResponse;
 import com.miml.ocr.service.OcrService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/ocr")
@@ -20,7 +23,9 @@ public class OcrController {
     }
 
 	@PostMapping("/extract")
-	public ResponseEntity<ApiResponse<String>> extractText(@RequestParam("data") byte[] data) throws Exception {
+	public ResponseEntity<ApiResponse<String>> extractText(@RequestPart("file") @Valid MultipartFile file) throws Exception {
+		
+		byte[] data = file.getBytes();
 		String extractedText = ocrService.extractTextFromImage(data);
 		return ApiResponse.toOkResponseEntity(extractedText);
 	}
