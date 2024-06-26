65535632922e265725174aa28b89f86a9ca48195
popup0221 <popup0221@naver.com>
2024-06-26 17:52:53 +0900

    fix: swagger https 옵션 변경

--- src/main/java/com/miml/config/SwaggerConfig.java
+++ src/main/java/com/miml/config/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.miml.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 /**
  * 스웨거 기본정보 설정
@@ -11,12 +15,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean(name="openAPI")
-    public OpenAPI openAPI() {
+	@Bean(name = "openAPI")
+	public OpenAPI openAPI() {
 
-        return new OpenAPI().info(new Info()
-                .title("Ballagain-API definition")
-                .version("v1")
-        );
-    }
+		Server server = new Server();
+		server.setUrl("https://api.ballagain.win/"); // https://에 접근 가능하게 설정
+
+		return new OpenAPI()
+				.info(new Info().title("Ballagain-API definition").version("v1"))
+				.servers(List.of(server));
+	}
 }
