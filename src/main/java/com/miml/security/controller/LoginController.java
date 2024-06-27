8d7f26347f861496cecaa8ad1cc52e2a91062c8b
popup0221 <popup0221@naver.com>
2024-06-27 22:04:16 +0900

    feat: login api 추가

---
+++ src/main/java/com/miml/security/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.miml.security.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/login")
+public class LoginController {
+
+	@PostMapping
+	public void login() {}
+}
